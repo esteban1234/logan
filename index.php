a80f52745853e07d10fbf128f9340506e13b3f44
esteban1234 <tva_libra@hotmail.com>
2017-09-18 14:02:39 -0500

    pagina lista

---
+++ index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>LOGAN INDUSTRIAL INTELIGENTE</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <META NAME="Title" content="LOGAN INDUSTRIAL INTELIGENTE">
+    <META NAME="Author" CONTENT="[http://loganindustrial.com/]"/>
+    <META name="Description" content="El mantenimiento y reparación de maquinaria pesada se enfoca en el conjunto de operaciones que técnicos profesionales hacen sobre la maquinaria para que esta cumpla adecuadamente con los propósitos sobre las cuales fue construida."/>
+   <META NAME="Keywords" CONTENT="montacargas, Bobcat, Liebherr, Hyundai, Crown, Rosenbauer, Trio, Kubota,  mantenimiento de maquinaria pesada, excavadoras, excavadora hidraulica, excavadoras, rodillo compactador, alquiler de maquinaria pesada, venta de cargadores frontales, venta de maquinaria pesada, elevadores de carga, motoniveladoras, cargador frontal, alquiler de maquinaria, empresas de maquinaria pesada, alquiler de montacargas, montacagas electricos, maquinaria para construccion, venta de montacargas,reparacion de maquinaria pesada, mantenimiento de maquinarias pesadas, mantenimiento de montacargas, mantenimiento de maquinaria, maquinaria construccion, venta de montacargas usados, repuestos maquinaria pesada, montacargas de combustion, montacargas a combustion."/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+<div class="bannerh" id="INICIO">
+  <header>
+    <figure class="logo pull-left">
+      <img src="img/logan.png" class="img-responsive" alt="LOGAN">
+    </figure>
+    <!-- <div class="op-menu">
+      <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+    </div> -->
+    <nav class="pull-right">
+      <ul>
+        <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO</a>
+        <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+        <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">SERVICIOS</a>
+        <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+      </ul>
+    </nav>
+    <div class="op-menu">
+      <i class="fa fa-bars fa-2x abrir" aria-hidden="true"></i>
+    </div>
+  </header>
+
+  <p class="text-banner">SOPORTES PARA MAQUINARIA <br> PESADA Y VIBRACIÓN.</p>
+  <div style="text-align: center; margin-top: 5%; margin-bottom: 5%; color: #fff;">
+    <a class="btn btn-warning btn-outline btn-lg" data-scroll href="#CONTACTO">CONTACTANOS</a>
+  </div>
+</div>
+
+
+
+  <span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+<section class="seccion1" id="NOSOTROS">
+<article class="somos">
+  <h1 style="color: #BA95C5;">LOGAN INDUSTRIAL INTELIGENTE</h1>
+  <p class="texto"><strong>LOGAN</strong> es una empresa especializada en la  asistencia técnica de maquinaria de industria, excavación y camiones de igual manera ocupa servicios en Administración y supervisión de construcción de inmuebles comerciales, institucionales y de servicios. Nuestro éxito se basa en conocer las necesidades de nuestros clientes y en proporcionarles el mejor servicio. <br><br>
+
+  Nos implicamos en los proyectos de nuestros clientes para garantizarles un servicio de calidad. En estos años hemos conseguido su confianza gracias a nuestros puntos fuertes. Asesoramiento y Servicio preventivo y correctivo.
+</p>
+</article>
+
+<div class="mvv">
+  <div class="section">
+      <div class="container" style="margin-top: 2%; margin-bottom: 7%;">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="text-center" style="color: #BA95C5;">¿Por que Elegir LOGAN?</h1>
+            <!-- <p class="text-center">We are a group of skilled individuals.</p> -->
+          </div>
+        </div> <br><br>
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 text-right ">
+            <i class="fa fa-usd fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4">
+            <h3 class="text-left" style="margin-top: 0;">Excelentes Precios</h3>
+            <p class="text-left">Precios competitivos en todos los servicios. ¡No hay cargos ocultos de ningún tipo!</p>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-2 text-right ">
+            <i class="fa fa-pencil-square-o fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4">
+            <h3 class="text-left" style="margin-top: 0;">Cotize Fácil Online</h3>
+            <p class="text-left"> Rellene nuestro formulario de contacto, y le enviaremos la cotización que usted necesita.</p>
+          </div>
+        </div> <br><br>
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 text-right ">
+            <i class="fa fa-clock-o fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4">
+            <h3 class="text-left">Períodos flexibles</h3>
+            <p class="text-left">Servicio disponible los 365 dias del año.</p>
+          </div>
+          <div class="col-xs-12 colsm-12 col-md-2 text-right ">
+            <i class="fa fa-phone fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4 text-center">
+            <h3 class="text-left">Asistencia telefónica</h3>
+            <p class="text-left">Estamos disponibles de lunes a viernes de 9 am a 6 pm.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+
+ <div class="container">
+   <div class="row margen">
+            <div class="col-sm-6 col-md-4 ">
+              <div class="iconos">
+                <i class="fa fa-check-square-o fa-3x fa-border " aria-hidden="true"></i>
+              </div>
+              <h2 class="text-center ">MISION</h2>
+              <p class="text-center ">Brindar a nuestros clientes servicios en el cumplimiento de obligaciones Fiscales, Contables y Administrativas, proporcionando los servicios de Consultoría y la Asesoría Fiscal; siempre con un alto nivel de experiencia profesional, conocimiento y compromiso.</p>
+            </div>
+            <div class="col-sm-6 col-md-4 ">
+              <div class="iconos">
+                <i class="fa fa-line-chart fa-3x fa-border " aria-hidden="true"></i>
+              </div>
+              <h2 class="text-center">VISION</h2>
+              <p class="text-center">Continuar como empresa pionera satisfaciendo las necesidades de nuestros clientes con la más alta calidad y a cumplir en forma veraz y en apego a las leyes vigentes los propósitos establecidos en los proyectos de trabajo.</p>
+            </div>
+            <div class="clearfix visible-sm-block"></div>
+            <div class="col-md-4">
+              <div class="iconos">
+                <i class="fa fa-handshake-o fa-3x fa-border " aria-hidden="true"></i>
+              </div>
+              <h2 class="text-center">VALORES</h2>
+              <p class="text-center">
+               Trabajo en equipo <br>
+                 Responsabilidad <br>
+                 Honestidad <br>
+                 Eficiencia <br>
+                 Respeto <br>
+                 Calidad
+              </p>
+            </div>
+          </div>
+ </div>
+
+    </section>
+  
+ 
+
+
+<section class="seccion2" id="SERVICIOS">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center" style="color: #fff;">SERVICIOS</h1>
+          <p class="lead text-center textt-nosotros" style="color: #fff;">Nos esforzamos por ofrecer a nuestros clientes el mejor servicio <br> para que tengan una experiencia exitosa con nosotros.</p>
+        </div>
+      </div> <br><br>
+       
+      <div class="row">
+      <div class="col-xs-12 col-sm-4 col-md-4 col-md-offset-2 card">
+        <a href="#">
+        <div class="thumbnail">
+            <div style="background-color:  #566573; height: 100px;" ></div>
+          <div class="caption">
+            <h3>Mantenimiento Preventivo</h3>
+            <hr>
+            <p>
+              Inspección periódica programada y planeada por los ingenieros encargados sobre el trabajo en todas las áreas de la planta con el fin de detectar el uso y desgaste de equipos y maquinas u otros y detectar posibles fallas o errores que puedan ocurrir en los lugares de trabajo.
+            </p>
+          </div>
+        </div>
+        </a>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 card">
+        <a href="#">
+        <div class="thumbnail">
+            <div style="background-color:  #566573; height: 100px;" ></div>
+          <div class="caption">
+            <h3>
+              Mantenimiento Full
+              <!-- <small>
+                Lorem ipsum dolor sit amet
+              </small> -->
+            </h3>
+            <hr>
+            <p>
+              Servicio de mantenimiento para su equipo ligero, pesado ó especializado manejando todas las marcas disponibles, entregando un analisis del estado de su equipo.
+            </p>
+            <!-- <button class="btn btn-default" role="button">View More</button> -->
+          </div>
+        </div>
+        </a>
+      </div>
+
+      </div> <br><br>
+
+      <div class="row">
+      <div class="col-xs-12 col-sm-4 col-md-4 col-md-offset-2 card">
+        <a href="#">
+        <div class="thumbnail">
+            <div style="background-color:  #566573; height: 100px;" ></div>
+          <div class="caption">
+            <h3>
+              Repuestos
+              <!-- <small>
+                Lorem ipsum dolor sit amet
+              </small> -->
+            </h3>
+            <hr>
+            <p>
+              Las marcas de los productos que distribuimos son fabricadas siguiendo rigurosos procedimientos de control de calidad, lo que nos permite ofrecer partes con estandares iguales a los del equipo original.
+            </p>
+            <!-- <button class="btn btn-default" role="button">View More</button> -->
+          </div>
+        </div>
+        </a>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 card">
+        <a href="#">
+        <div class="thumbnail">
+            <div style="background-color:  #566573; height: 100px;" ></div>
+          <div class="caption">
+            <h3>
+              Servicio Técnico
+              <!-- <small>
+                Lorem ipsum dolor sit amet
+              </small> -->
+            </h3>
+            <hr>
+            <p>
+              Nuestro grupo humano está capacitado y certificado en las fábricas de producción y provisto de los mejores equipos y herramientas, para solucionar de manera inmediata cualquier inconveniente que presente una máquina.
+            </p>
+            <!-- <button class="btn btn-default" role="button">View More</button> -->
+          </div>
+        </div>
+        </a>
+      </div>
+  </div>
+</section>
+
+<section class="seccion3" id="CONTACTO">
+     <figure class="banner3">
+  </figure> <br><br><br>
+  <div class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center text-danger text-dise-nosotros" style="color: #BA95C5;">CONTACTO</h1> <br><br><br>
+
+          <p class="lead text-center">Encuentranos cerca de ti, teléfonos, horarios, direcciones y contacto.</p>
+        </div>
+      </div> <br>
+
+      <div class="mapa">
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1862.2794333784682!2d-89.62092134207455!3d21.01031299650211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5676a5f0bd43db%3A0xa172771e0ff815fc!2sCalle+7A+279E%2C+Campestre%2C+97120+M%C3%A9rida%2C+Yuc.!5e0!3m2!1ses-419!2smx!4v1505760197950" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div> <br><br><br>
+
+      <div class="row">
+          <div class="col-sm-6 col-md-3 br-contacto">
+            <div class="iconos">
+              <i class="fa fa-clock-o fa-3x fa-border" aria-hidden="true"></i>
+            </div>
+            <h2 class="text-center">Horario</h2>
+            <p class="text-center">Lunes a Viernes de 9 am a 6 pm</p>
+          </div>
+          <div class="col-sm-6 col-md-3 br-contacto">
+            <div class="iconos">
+              <i class="fa fa-envelope-o fa-3x fa-border" aria-hidden="true"></i>
+            </div>
+            <h2 class="text-center">Correo</h2>
+            <p class="text-center">info@loganindustrial.com</p>
+          </div>
+          <div class="col-sm-6 col-md-3 br-contacto">
+            <div class="iconos">
+              <i class="fa fa-phone fa-3x fa-border" aria-hidden="true"></i>
+            </div>
+            <h2 class="text-center">Teléfono</h2>
+            <p class="text-center">Pendiente
+            </p>
+          </div>
+
+          <div class="col-sm-6 col-md-3 br-contacto">
+            <div class="iconos">
+              <i class="fa fa-map-marker fa-3x fa-border" aria-hidden="true"></i>
+            </div>
+            <h2 class="text-center">Dirección</h2>
+            <p class="text-center">C. 7A #279E por C. 40 y C. 38 Col. CAMPESTRE, MERIDA, YUCATAN. C.P.97120.
+            </p>
+          </div>
+        </div> <br><br>
+    </div>
+  </div>
+  
+</section>
+
+
+
+
+<section id="contact" class="content-section">
+      <div class="section-content">
+        <h1 class="section-header  text-center">FORMULARIO</h1>
+        <h3 class="text-center">Escribenos tus comentario, dudas o preguntas, estamos para servirte.</h3>
+      </div>
+      <div class="contact-section">
+      <div class="container">
+        <form>
+          <div class="col-md-6 form-line">
+              <div class="form-group">
+                <label for="exampleInputUsername">Nombre completo</label>
+                <input type="text" class="form-control" id="txtNOMBRE" placeholder="Escribe tu nombre completo">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail">Teléfono</label>
+                <input type="email" class="form-control" id="txtTELEFONO" placeholder="Escribe tu teléfono">
+              </div>  
+              <div class="form-group">
+                <label for="telephone">Correo</label>
+                <input type="tel" class="form-control" id="txtCORREO" placeholder="Escribe tu correo eléctronico">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for ="description"> Comentario</label>
+                <textarea  class="form-control" id="txtCOMENTARIO" placeholder="Escribe tu nombre completo"></textarea>
+              </div>
+              <div>
+
+              
+              
+                <div id="_AJAX_PRE_" ></div>
+                <button type="submit" class="btn btn-default submit" onclick="sendCORREO()"><i class="fa fa-paper-plane" aria-hidden="true"></i>  ENVIAR</button>
+              </div>
+              
+          </div>
+        </form>
+      </div>
+    </section>
+
+<footer>
+<p>Derechos de autor 2017 © | LOGAN INDUSTRIAL INTELIGENTE S. DE R.L. DE C.V. </p>
+</footer>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+</html>
